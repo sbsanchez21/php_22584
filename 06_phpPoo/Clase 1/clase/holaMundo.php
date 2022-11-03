a5f2a951e37ae3c8df369811819b1cdcd0aa4c1a
santos <santos.sanchez@bue.edu.ar>
2022-11-03 12:10:17 -0300

    phpPoo Clase1

---
+++ 06_phpPoo/Clase 1/clase/holaMundo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/holaMundo.css">
+</head>
+<body>
+    <p onclick="saludo()">Hola Mundo</p>
+
+    <?php 
+        echo "<p>Hola PHP</p>";
+
+        //conectar db, traer listado alumnos
+        //generar una tabla dinamicamente
+    ?>
+    
+    <script src="js/holaMundo.js"></script>
+</body>
+</html>
